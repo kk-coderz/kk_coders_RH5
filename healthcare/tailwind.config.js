ee8be06cb3e413247edfe9e54f31381144a530cb
Gobaluyaka <himathsamarakoon@gmail.com>
2024-04-21 01:59:21 +0530

    reverted tailwind cofig

--- healthcare/tailwind.config.js
+++ healthcare/tailwind.config.js
@@ -1,19 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/*/.{html,ts}",
+    "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {
-      backgroundColor: {
-        'custom-primary': '#5DEBD7',
-        'custom-secondary': '#1679AB',
-        'custom-tertiary': '#074173',
-      },
-      ringColor: {
-        'custom-border' : '#C5FF95',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
