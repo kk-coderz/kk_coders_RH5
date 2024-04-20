d8ea5d610b7768fce9f3c1848fd9c99a8988e578
Gobaluyaka <himathsamarakoon@gmail.com>
2024-04-21 00:04:59 +0530

    chaged some designs

--- healthcare/src/app/components/login/login.component.ts
+++ healthcare/src/app/components/login/login.component.ts
@@ -24,52 +24,7 @@ export class LoginComponent {
     email : new FormControl(""),
     password : new FormControl("")
   })
-  // ngOnInit() {
-  //   console.log(this.authService.getToken())
-  //   if (this.token == "" && isPlatformBrowser(this.platformId)) {
-  //     // @ts-ignore
-  //     window.onGoogleLibraryLoad = () => {
-  //       console.log('Google\'s One-tap sign in script loaded!');
 
-  //       // @ts-ignore
-  //       google.accounts.id.initialize({
-  //         // Ref: https://developers.google.com/identity/gsi/web/reference/js-reference#IdConfiguration
-  //         client_id: "638151064556-6o7bc48rmsqr6f4hhlek80ocdvfpia55.apps.googleusercontent.com",
-  //         callback: this.handleCredentialResponse.bind(this), // Whatever function you want to trigger...
-  //         auto_select: true,
-  //         cancel_on_tap_outside: false
-  //       });
-
-  //       // OPTIONAL: In my case I want to redirect the user to an specific path.
-  //       // @ts-ignore
-  //       google.accounts.id.prompt((notification: PromptMomentNotification) => {
-  //         console.log('Google prompt event triggered...');
-
-  //         if (notification.getDismissedReason() === 'credential_returned') {
-  //           this.ngZone.run(() => {
-  //             this.router.navigate(['/dashboard'], { replaceUrl: true });
-  //             console.log('Welcome back!');
-  //           });
-  //         }
-  //       });
-  //     };
-  //   }
-
-  // }
-
-  // handleCredentialResponse(response: CredentialResponse) {
-  //   // Decoding  JWT token...
-  //     let decodedToken: any | null = null;
-  //     try {
-  //       this.token = response?.credential.split('.')[1]
-  //       decodedToken = JSON.parse(atob(this.token));
-  //     } catch (e) {
-  //       console.error('Error while trying to decode token', e);
-  //     } finally {
-  //       this.authService.setToke(this.token)
-  //     }
-  //     console.log('decodedToken', decodedToken);
-  //   }
   handleClick() {
     this.authService.login(
       this.formData.controls["email"].value,
