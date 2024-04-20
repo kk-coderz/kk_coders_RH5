e469ee489f985bbcf135dc0b9276ead12e50770e
Gobaluyaka <himathsamarakoon@gmail.com>
2024-04-21 01:54:11 +0530

    added gradient and removed .angular from repo

--- healthcare/tailwind.config.js
+++ healthcare/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/*/.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'custom-primary': '#5DEBD7',
+        'custom-secondary': '#1679AB',
+        'custom-tertiary': '#074173',
+      },
+      ringColor: {
+        'custom-border' : '#C5FF95',
+      },
+    },
   },
   plugins: [],
 }
-
